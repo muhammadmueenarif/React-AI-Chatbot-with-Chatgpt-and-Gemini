3a4e196b9c8b629bcdd8c97fba9b45deb1d989ac
Mueen Arif <muhammadmueenarif786@gmail.com>
2024-12-07 19:07:45 +0500

    added some styling. app.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-
+import styles from './App.module.css'
 function App() {
   return (
-   <div>AI Chatbot</div>
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="/chat-bot.png" />
+        <h2 className={styles.Title}>AI Chatbot</h2>
+      </header>
+      <div className={styles.ChatContainer}/>
+    </div>
   )
 }
 
