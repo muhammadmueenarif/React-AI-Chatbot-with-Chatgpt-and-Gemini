9c5edcf1c46e320ee4c7c33f559d56188b1a919a
Mueen Arif <muhammadmueenarif786@gmail.com>
2024-12-07 20:38:17 +0500

    text area to enter message and send icon button added and styled css file

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,10 +1,21 @@
+import styles from './Controls.module.css'
 export function Controls() {
     return (
-        <div>
-            <div>
-                <textarea placeholder="Message AI Chatbot"/>
+        <div className={styles.Controls}>
+            <div className={styles.TextAreaContainer}>
+                <textarea className={styles.TextArea} placeholder="Message AI Chatbot" />
             </div>
-            <button>Send</button>
+            <button className={styles.Button}>
+                <SendIcon/>
+            </button>
         </div>
+    )
+}
+
+function SendIcon() {
+    return (
+        <svg width="24px" height="24px" viewBox="0 -960 960 960" fill="#5f6368" xmlns="http://www.w3.org/2000/svg">
+            <path d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z" />
+        </svg>
     )
 }
