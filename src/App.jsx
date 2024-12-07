4b9692bb380d5d6eaa3e5f2fa2cf8bb7f8bcb0e3
Mueen Arif <muhammadmueenarif786@gmail.com>
2024-12-07 19:36:10 +0500

    chat msg added. send it by props to chat component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,62 @@
+import { Chat } from './components/Chat/Chat'
 import styles from './App.module.css'
+import { useState } from 'react'
 function App() {
+  const [messages, setMessages] = useState(MESSAGES);
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
         <img className={styles.Logo} src="/chat-bot.png" />
         <h2 className={styles.Title}>AI Chatbot</h2>
       </header>
-      <div className={styles.ChatContainer}/>
+      <div className={styles.ChatContainer}>
+        {/* sending props */}
+        <Chat messages={messages}/>
+      </div>
     </div>
   )
 }
+
+const MESSAGES =[ {
+  role: 'user',
+  content: 'Hello',
+}, 
+  {
+    role: 'assistant',
+    content: 'Hey what can I assist you with?',
+  },
+  {
+    role: 'user',
+    content: 'Hello',
+  }, 
+    {
+      role: 'assistant',
+      content: 'Hey what can I assist you with?',
+    },
+    {
+      role: 'user',
+      content: 'Hello',
+    }, 
+      {
+        role: 'assistant',
+        content: 'Hey what can I assist you with?',
+      },
+      {
+        role: 'user',
+        content: 'Hello',
+      }, 
+        {
+          role: 'assistant',
+          content: 'Hey what can I assist you with?',
+        },
+        {
+          role: 'user',
+          content: 'Hello',
+        }, 
+          {
+            role: 'assistant',
+            content: 'Hey what can I assist you with?',
+          },
+]
 
 export default App
---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,11 @@
+export function Chat({ messages }) {
+    return (
+        <div>
+            {messages.map(({ role, content }, index) => {
+                <div key={index} data-role={role}>
+                    {content}
+                </div>
+            })}
+        </div>
+    )
+}
